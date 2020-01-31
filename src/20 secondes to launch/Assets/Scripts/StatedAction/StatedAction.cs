4fc86eea73664cc6b3bc7e191d7f2658211d3005
Tristan Guichaoua <guichaoua.tristan@gmail.com>
2020-01-31 23:29:42 +0100

    VALVE

---
+++ src/20 secondes to launch/Assets/Scripts/StatedAction/StatedAction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StatedAction<T, TEvent> : MonoBehaviour where TEvent : UnityEvent<T>
+{
+    [SerializeField] public T[] states;
+    [SerializeField] public TEvent then;
+
+    private int currentIndex;
+
+    public void Do()
+    {
+        if (states.Length == 0)
+            return;
+        currentIndex %= states.Length;
+        then.Invoke(states[currentIndex++]);
+    }
+}
---
+++ src/20 secondes to launch/Assets/Scripts/UnityEvents.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class UnityEventSprite : UnityEvent<Sprite> { }
+
+[Serializable]
+public class UnityEventInt : UnityEvent<int> { }
+
+[Serializable]
+public class UnityEventFloat : UnityEvent<float> { }
