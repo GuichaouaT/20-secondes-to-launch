10d08f7ff4deb3086e5e037392a3cb35136ed903
Tristan Guichaoua <guichaoua.tristan@gmail.com>
2020-01-31 21:22:36 +0100

    Prototyping button cilck + timer

---
+++ src/20 secondes to launch/Assets/Scripts/EventListener.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventListener : MonoBehaviour
+{
+    [SerializeField] private EventTrigger[] triggers;
+    [SerializeField] private UnityEvent onTriggered;
+
+    private void OnEnable()
+    {
+        foreach (var trigger in triggers)
+            trigger.Subscribe(this);
+    }
+
+    private void OnDisable()
+    {
+        foreach (var trigger in triggers)
+            trigger.Unsubscribe(this);
+    }
+
+    public void Trigger()
+    {
+        onTriggered.Invoke();
+    }
+}
---
+++ src/20 secondes to launch/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LevelManager : MonoBehaviour
+{
+    [Header("Componants")]
+    [SerializeField] private TextMeshPro countdownText;
+
+    [Header("Settings")]
+    [SerializeField] private int initialCountdown = 20;
+
+    #region Unity Callbacks
+
+    private void Start()
+    {
+        StartCoroutine(Routine_Countdown());
+    }
+
+    #endregion
+
+    #region Event Listener
+
+    public void EVENT_Lauch()
+    {
+        Lauch();
+    }
+
+    #endregion
+
+
+    #region Routine
+
+    private IEnumerator Routine_Countdown()
+    {
+        for (int i = initialCountdown; i >= 0; i--)
+        {
+            countdownText.SetText(i.ToString());
+            yield return new WaitForSeconds(1);
+        }
+
+        Lauch();
+    }
+
+    #endregion
+
+    private void Lauch()
+    {
+        Debug.Log("LAUNCH !!!!!!!!");
+    }
+
+}
