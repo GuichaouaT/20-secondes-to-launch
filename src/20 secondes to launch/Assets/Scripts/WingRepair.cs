c45e141a5377072a12e23d3ee97f36a244ed28b2
Tristan Guichaoua <guichaoua.tristan@gmail.com>
2020-02-01 22:13:21 +0100

    REPAIR WING git add -A

---
+++ src/20 secondes to launch/Assets/Scripts/WingRepair.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WingRepair : MonoBehaviour
+{
+
+    [SerializeField] private GameObject wingAloneObject;
+    [SerializeField] private new Animation animation;
+    [Header("Animation Name")]
+    [SerializeField] private string canRepairAnimationName;
+    [SerializeField] private string noWingAnimationName;
+    [Header("Event")]
+    [SerializeField] private UnityEvent onWingRepaired;
+
+    #region EVENT LISTENER
+
+    public void EVENT_WingRepaired()
+    {
+        onWingRepaired.Invoke();
+    }
+
+    #endregion
+
+    public void TryRepair()
+    {
+        if (wingAloneObject.activeSelf)
+        {
+            animation.Play(canRepairAnimationName);
+        }
+        else
+        {
+            animation.Play(noWingAnimationName);
+        }
+    }
+}
