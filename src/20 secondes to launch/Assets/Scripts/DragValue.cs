0c66fb1f69a0cd4a067c6dd5d5befc98c8ec9dab
Tristan Guichaoua <guichaoua.tristan@gmail.com>
2020-02-01 21:46:57 +0100

    Levier !

--- src/20 secondes to launch/Assets/Scripts/DragValue.cs
+++ src/20 secondes to launch/Assets/Scripts/DragValue.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class DragValue : MonoBehaviour
 {
+ 
     public float value;
     public float min;
     public float max;
     public float deltaByDistance;
+    public bool resetOnRelease = false;
+    public float resetValue = 0;
 
     [SerializeField] private UnityEventFloat onValue;
 
@@ -24,12 +27,23 @@ public class DragValue : MonoBehaviour
         lastMousePos = Input.mousePosition.x;
     }
 
+    private void OnMouseUp()
+    {
+        if (resetOnRelease)
+            SetValue(resetValue);
+    }
+
     private void OnMouseDrag()
     {
         var diff = Input.mousePosition.x - lastMousePos;
         value += diff * deltaByDistance;
-        value = Mathf.Clamp(value, min, max);
         lastMousePos = Input.mousePosition.x;
+        SetValue(value);
+    }
+
+    private void SetValue(float value)
+    {
+        this.value = Mathf.Clamp(value, min, max);
         onValue.Invoke(value);
     }
 }
--- src/20 secondes to launch/Assets/Scripts/ValueIncrease.cs
+++ src/20 secondes to launch/Assets/Scripts/ValueIncrease.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class ValueIncrease : MonoBehaviour
 {
     public float value;
+    public float min;
+    public float max;
     public float speed;
     [SerializeField] private UnityEventFloat onValue;
 
     public float Speed { get => speed; set => speed = value; }
 
+    private void OnValidate()
+    {
+        if (min > max)
+            min = max;
+    }
+
     private void Update()
     {
         value += Time.deltaTime * speed;
+        value = Mathf.Clamp(value, min, max);
         onValue.Invoke(value);
     }
 }
