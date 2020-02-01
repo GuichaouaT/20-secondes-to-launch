60fd71983b62de9f19db1438842d924d629b86db
j8guyade <j8guyade@enib.fr>
2020-02-01 19:57:16 +0100

    gauge + script gauge

---
+++ src/20 secondes to launch/Assets/Scripts/GaugeBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GaugeBehaviour : MonoBehaviour
+{
+    [SerializeField] private Transform pointer;
+    [Range(0,1)] public float pos = 0;
+    private float angle = 0;
+
+
+
+    private void Update()
+    {
+        if (angle != -pos * 180)
+            angle = -pos * 180;
+        if (pointer.transform.eulerAngles[2] != angle)        
+            pointer.transform.Rotate(0, 0, angle- pointer.transform.eulerAngles[2]);
+    }
+}
