393b867c68d7c7448f2ae77159541c741f13bf17
Tristan Guichaoua <guichaoua.tristan@gmail.com>
2020-02-01 18:04:43 +0100

    Thermomètre

---
+++ src/20 secondes to launch/Assets/Scripts/DelayedAction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DelayedAction : MonoBehaviour
+{
+    public float delay;
+    [SerializeField] private UnityEvent action;
+
+    public void Do()
+    {
+        StartCoroutine(Routine());
+    }
+
+    private IEnumerator Routine()
+    {
+        yield return new WaitForSeconds(delay);
+        action.Invoke();
+    }
+}
