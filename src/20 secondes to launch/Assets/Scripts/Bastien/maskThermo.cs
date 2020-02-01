14dd8d32bd2c227db56d9a8dadcbd09b3780812c
Gala-2020 <47535482+Arcadeuh@users.noreply.github.com>
2020-02-01 01:35:18 +0100

    Thermometre et autre

---
+++ src/20 secondes to launch/Assets/Scripts/Bastien/maskThermo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class maskThermo : MonoBehaviour
+{
+    private Transform trMask;
+    public float posDeltaY = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trMask = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = new Vector3(transform.localPosition.x, posDeltaY, transform.localPosition.z);
+    }
+}
