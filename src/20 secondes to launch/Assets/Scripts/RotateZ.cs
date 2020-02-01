c0b1b20bd08a2e83ea90cdf454bf483883a5b5f3
Tristan Guichaoua <guichaoua.tristan@gmail.com>
2020-02-01 21:30:49 +0100

    levier

---
+++ src/20 secondes to launch/Assets/Scripts/RotateZ.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateZ : MonoBehaviour
+{
+    public void Rotate(float angle)
+    {
+        var euler = transform.localEulerAngles;
+        euler.z = angle;
+        transform.localEulerAngles = euler;
+    }
+}
